4363929e051174bdd7a3d64744477300dbc3009f
feliciahf <70622461+feliciahf@users.noreply.github.com>
2020-10-28 12:18:00 +0100

    goodreads-master

--- GoodReads_API_2.py
+++ GoodReads_API_2.py
@@ -1,5 +1,7 @@
-sudo pip install xmltodict requests rauth
-sudo python setup.py install
+## Terminal
+#sudo pip install xmltodict requests rauth
+#cd goodreads-master
+#sudo python setup.py install
 
 from goodreads import client
 gc = client.GoodreadsClient(<hZjlNpK7O5hhgfbClCZLQ>, <xRi7TiOUkGGlH3Qxrc3A7sadXEH8EPffUvw2dghye4>)
