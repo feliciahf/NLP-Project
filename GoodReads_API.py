1ddb9dd23587539f11f3632e8fd6b01f0510b802
feliciahf <70622461+feliciahf@users.noreply.github.com>
2020-11-01 22:21:39 +0100

    imported API key & secret

--- GoodReads_API.py
+++ GoodReads_API.py
@@ -1,10 +1,20 @@
-## Terminal
+## Terminal: setup modules to be used
 #sudo pip install xmltodict requests rauth
-#cd goodreads-master
+#cd goodreadsmaster
 #sudo python setup.py install
 
+# import API key & secret
 from goodreadsmaster.goodreads import client
-gc = client.GoodreadsClient(<hZjlNpK7O5hhgfbClCZLQ>, <xRi7TiOUkGGlH3Qxrc3A7sadXEH8EPffUvw2dghye4>)
+gc = client.GoodreadsClient('hZjlNpK7O5hhgfbClCZLQ', 'xRi7TiOUkGGlH3Qxrc3A7sadXEH8EPffUvw2dghye4')
 
+# get title of book with id 1
 book = gc.book(1)
 
+# get titles & shelves (hopefully)
+from goodreadsmaster.goodreads import book
+gb = book.GoodreadsBook(book_dict, client) # what is book_dict?? client = gc ?
+
+gb = book.GoodreadsBook(1, gc) # does not work
+
+title = gb.title
+shelves = gb.popular_shelves
